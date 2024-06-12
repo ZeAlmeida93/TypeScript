ab1a6c43de1690ec26165a2c431a316761ea1c08
Jose Almeida <gralhastudio@gmail.com>
2024-06-12 20:55:47 +0100

    js file creation

---
+++ types.js
@@ -0,0 +1,4 @@
+var int = 1000;
+var float = 10.10;
+var hex = 0xf00d;
+var octal = 484;
---
+++ types.ts
@@ -0,0 +1,7 @@
+const int: number = 1000;
+const float: number = 10.10;
+const hex:number = 0xf00d;
+const octal:number = 0o744;
+
+
+
